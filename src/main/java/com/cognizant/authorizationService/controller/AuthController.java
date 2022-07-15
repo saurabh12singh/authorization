50fcb643c5026e55579841462ec67beeaa1cd4cb
saurabh12singh <61622700+saurabh12singh@users.noreply.github.com>
2022-07-15 14:52:12 +0530

    Update AuthController.java

--- src/main/java/com/cognizant/authorizationService/controller/AuthController.java
+++ src/main/java/com/cognizant/authorizationService/controller/AuthController.java
@@ -23,7 +23,7 @@ import com.cognizant.authorizationService.service.JwtUtil;
  * getting the token and validating the token this class will be used.
  */
 @RestController
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("http://mfpe-portfolio-pod5.s3-website-us-west-2.amazonaws.com")
 public class AuthController {
 	private static Logger logger = LoggerFactory.getLogger(AuthController.class);
 
